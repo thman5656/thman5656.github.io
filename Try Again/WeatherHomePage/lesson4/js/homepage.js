62e78c100e06d19328bb244cbfb1b98ca2021016
thman5656 <hom17001@byui.edu>
2020-10-17 16:18:00 -0600

    contact us

--- Try Again/WeatherHomePage/lesson4/js/homepage.js
+++ Try Again/WeatherHomePage/lesson4/js/homepage.js
@@ -2,16 +2,26 @@
 //need to use JS to show date in the footer 
 
 window.onload = function(){lastModDate()}
+window.onload = function(){PopupMessage()}
 function lastModDate()
-{
-  const CurrentDate = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-document.getElementById("lastModDate").innerHTML = new Date().toLocaleDateString("en-US", CurrentDate);
-}
+  {
+    const CurrentDate = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+  document.getElementById("lastModDate").innerHTML = new Date().toLocaleDateString("en-US", CurrentDate);
+  }
 
 function toggleMenu()
-{ 
-let curmenu = document.getElementById('navigation').classList;
-if (curmenu == "noshow"){document.getElementById('navigation').setAttribute("class","shownav");}
-else if (curmenu == "shownav"){document.getElementById('navigation').setAttribute("class","noshow");}  
-else{window.alert("stupid");}
-}
+  { 
+  let curmenu = document.getElementById('navigation').classList;
+  if (curmenu == "noshow"){document.getElementById('navigation').setAttribute("class","shownav");}
+  else if (curmenu == "shownav"){document.getElementById('navigation').setAttribute("class","noshow");}  
+  else{window.alert("stupid");}
+  }
+
+function PopupMessage()
+  {
+    let today = new Date();
+    let daynumber = today.getDay();
+
+    if(daynumber == 6){document.getElementById('Message').setAttribute("Class", "show")}
+    else {document.getElementById("Message").setAttribute("class","noshow")}
+  }
