f7983899b67bf4930c516efbb68abed664382b61
thman5656 <hom17001@byui.edu>
2020-10-26 21:21:41 -0600

    wierd fix

--- Try Again/WeatherHomePage/lesson4/js/homepage.js
+++ Try Again/WeatherHomePage/lesson4/js/homepage.js
@@ -3,7 +3,7 @@
 
 function toggleMenu()
   { 
-    alert(daynumber );
+
   let curmenu = document.getElementById('navigation').classList;
   if (curmenu == "noshow"){document.getElementById('navigation').setAttribute("class","shownav");}
   else if (curmenu == "shownav"){document.getElementById('navigation').setAttribute("class","noshow");}  
