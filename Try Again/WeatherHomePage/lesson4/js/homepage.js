15e4e0cb2f3b6c959c5d9e6e9bee40cc33394a7d
thman5656 <hom17001@byui.edu>
2020-10-26 21:36:22 -0600

    stry tag fix

--- Try Again/WeatherHomePage/lesson4/js/homepage.js
+++ Try Again/WeatherHomePage/lesson4/js/homepage.js
@@ -7,5 +7,5 @@ function toggleMenu()
   let curmenu = document.getElementById('navigation').classList;
   if (curmenu == "noshow"){document.getElementById('navigation').setAttribute("class","shownav");}
   else if (curmenu == "shownav"){document.getElementById('navigation').setAttribute("class","noshow");}  
-  else{window.alert("stupid");}
+  else{window.alert("stupid"); }
   }
